1d53d5d701a1e9e3dcb903c5062d3a91c3dac1d9
reskyadhyaksa <reskyadhyaksa19@gmail.com>
2023-07-26 17:27:13 +0700

    UPDATE DASHBOARD 25% (apa ini bingung bacanya :))

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -1,29 +1,59 @@
 import { Card } from "@material-tailwind/react";
 import { Typography } from "@material-tailwind/react";
-import { BsFillPersonFill } from 'react-icons/bs'
+import { BsArrowDownRightSquareFill, BsArrowUpRightSquareFill, BsFillPersonFill } from 'react-icons/bs'
 
 export default function DashboardContent() {
+    const dashboard_data = {
+        total_visitor: 243521,
+        stock_available: 24500,
+        total_link_visited: 342560,
+    }
     return(
         <>
-            <div className="flex flex-col ml-5">
+            <div className="flex flex-col ml-16 mr-16">
                 <div>
                     <Typography className="mt-5 font-lato font-bold text-2xl text-black">Dashboard</Typography>
                     <hr className="h-px my-3 bg-gray-700 border-0 dark:bg-gray-700 mr-5"></hr>
                 </div>
-                <div id="main" className="flex font-lato">
+                <div id="main" className="flex md:flex-row flex-col">
                     <div id="row1" className="flex flex-col">
-                        <div id="rcolumn1" className="flex">
-                            <Card id="card1" className="h-[125px] w-[200px] mr-12 bg-[#A88AD4] p-4 text-white">
+                        <div id="rcolumn1" className="flex md:flex-row flex-col">
+                            <Card id="card1" className="h-[100px] w-[200px] md:mr-12 bg-[#A88AD4] p-4 mb-5 self-center md:self-start mr-5">
                                 <div className="flex flex-col">
                                     <div className="flex flex-row">
-                                        <div className="p-1 bg-white rounded-lg"><BsFillPersonFill className="h-4 w-4 fill-black"/></div>
-                                        <div className="text-white font-bold font-lato">Total Visitior</div>
+                                        <div className="p-1 h-[26px] bg-white rounded-lg md:mr-6"><BsFillPersonFill className="h-4 w-4 fill-black"/></div>
+                                        <Typography className="font-lato font-bold text-black md:text-md text-sm">Total Visitor</Typography>
+                                    </div>
+                                    <div className="flex flex-row items-center mt-3">
+                                        <Typography color="black" className="font-lato font-bold mr-auto">{dashboard_data.total_visitor}</Typography>
+                                        <BsArrowDownRightSquareFill color="#EB1F39" className="bg-white rounded-md w-5 h-5 outline-double"/>
                                     </div>
                                 </div>
-                                <div className="flex flex-col">2</div>
                             </Card>
-                            <Card id="card2" className="h-[125px] w-[200px] mr-12">2</Card>
-                            <Card id="card3" className="h-[125px] w-[200px]">3</Card>
+                            <Card id="card1" className="h-[100px] w-[200px] md:mr-12 bg-[#B9E303] p-4 mb-5 self-center md:self-start mr-5">
+                                <div className="flex flex-col">
+                                    <div className="flex flex-row">
+                                        <div className="p-1 h-[26px] bg-white rounded-lg mr-4"><BsFillPersonFill className="h-4 w-4 fill-black"/></div>
+                                        <Typography className="font-lato font-bold text-black">Stock Available</Typography>
+                                    </div>
+                                    <div className="flex flex-row items-center mt-4">
+                                        <div className="font-lato text-black font-bold mr-auto">{dashboard_data.stock_available}</div>
+                                        <BsArrowUpRightSquareFill color="#1FEB4C" className="bg-white rounded-md w-5 h-5 outline-double"/>
+                                    </div>
+                                </div>
+                            </Card>
+                            <Card id="card1" className="h-[100px] w-[200px] bg-[#B5D5E1] p-4 mb-5 self-center md:self-start mr-5">
+                                <div className="flex flex-col">
+                                    <div className="flex flex-row">
+                                        <div className="p-1 bg-white rounded-lg mr-3"><BsFillPersonFill className="h-4 w-4 fill-black"/></div>
+                                        <Typography className="text-black font-lato font-bold">Total Link Visited</Typography>
+                                    </div>
+                                    <div className="flex flex-row items-center mt-4">
+                                        <div className="text-black font-lato font-bold mr-auto">{dashboard_data.total_link_visited}</div>
+                                        <BsArrowDownRightSquareFill color="#EB1F39" className="bg-white rounded-md w-5 h-5 outline-double"/>
+                                    </div>
+                                </div>
+                            </Card>
                         </div>
                         <Card id="rcolumn2" color="cyan" className="mb-[26px] mt-[26px]">Column2</Card>
                         <Card id="rcolumn3" color="cyan">Column3</Card>
