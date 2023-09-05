bf0d1f3dff404e7640aae8117af15051d7662018
reskyadhyaksa <reskyadhyaksa19@gmail.com>
2023-09-05 14:48:59 +0700

    new branch

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,9 @@ export default function App() {
   const handleLoginClick = () => {
     setContent('Dashboard');
   };
+  
 
+  /** App */
   return (
     <div>
       {content === 'Dashboard' && (
